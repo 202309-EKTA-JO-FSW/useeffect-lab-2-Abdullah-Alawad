409e76200ca367abc96080cf5dd950873d48a4f6
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-11-03 20:41:27 +0300

    Tests are Done

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,10 +1,19 @@
-export default function Form() {
+
+
+export default function Form({userName , setUsername}) {
+
+  function handleOnChange(event){
+    event.preventDefault();
+    const {name, value} = event.target;
+    setUsername(value);
+  }
+
   return (
-    <form>
+    <form >
       <h3>Search user Github</h3>
       <div className="mb-3">
         <label>User name</label>
-        <input type="text" className="form-control" placeholder="User Name" />
+        <input onChange={(e) => handleOnChange(e)} value={userName} type="text" className="form-control" placeholder="User Name" />
       </div>
     </form>
   );
